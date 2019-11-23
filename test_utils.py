3f052bfab2700eec72cc14dacb54782e5f209a90
Tiep Vu <vuhuutiep@gmail.com>
2019-11-22 18:17:37 -0800

    block_comment: testing header

--- test_utils.py
+++ test_utils.py
@@ -58,12 +58,13 @@ class TestBlockComment(object):
     def test_with_header(self):
         input_md = './tests/input_4.md'
         output_md = './tests/output_4.md'
-        with tempfile.NamedTemporaryFile() as temp:
-            block_comment(input_md, temp.name)
-            print(filecmp.cmp(output_md, temp.name))
-            assert filecmp.cmp(output_md, temp.name), compare_files(
-                output_md, temp.name
-            )
+        temp_ouput_md = './tests/temp_ouput.md'
+        # with tempfile.NamedTemporaryFile() as temp:
+        block_comment(input_md, temp_ouput_md)
+        print(filecmp.cmp(output_md, temp_ouput_md))
+        assert filecmp.cmp(output_md, temp_ouput_md), compare_files(
+            output_md, temp_ouput_md
+        )
 
     # def test_with_code_block(self):
     #     input_md = './tests/input_5.md'
