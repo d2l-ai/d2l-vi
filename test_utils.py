68aa6ca6e2f98271f2bd28d0e21a374e18fa1e8d
tiepvupsu <vuhuutiep@gmail.com>
2019-11-23 18:45:51 -0800

    Utils header (#12)

--- test_utils.py
+++ test_utils.py
@@ -15,6 +15,7 @@ def compare_files(filename1, filename2):
         for line1, line2 in zip(file1, file2):
             cnt += 1
             if line1 != line2:
+                print(filename1)
                 print('line {}: {} != {}'.format(cnt, line1, line2))
                 return False
         if file1 or file2:
@@ -24,53 +25,41 @@ def compare_files(filename1, filename2):
 
 
 class TestBlockComment(object):
-    def test_1(self):
-        input_md = './tests/input_1.md'
-        output_md = './tests/output_1.md'
+    def check(self, input_md, output_md):
         temp_file_md = './tests/temp_ouput.md'
         block_comment(input_md, temp_file_md)
         print(filecmp.cmp(output_md, temp_file_md))
         assert filecmp.cmp(output_md, temp_file_md), compare_files(
             output_md, temp_file_md
         )
+
+    def test_1(self):
+        input_md = './tests/input_1.md'
+        output_md = './tests/output_1.md'
+        self.check(input_md, output_md)
 
     def test_2(self):
         """multiple blank lines"""
         input_md = './tests/input_2.md'
         output_md = './tests/output_2.md'
-        temp_file_md = './tests/temp_ouput.md'
-        block_comment(input_md, temp_file_md)
-        print(filecmp.cmp(output_md, temp_file_md))
-        assert filecmp.cmp(output_md, temp_file_md), compare_files(
-            output_md, temp_file_md
-        )
+        self.check(input_md, output_md)
 
     def test_with_double_hythen(self):
         input_md = './tests/input_3.md'
         output_md = './tests/output_3.md'
-        temp_file_md = './tests/temp_ouput.md'
-        block_comment(input_md, temp_file_md)
-        print(filecmp.cmp(output_md, temp_file_md))
-        assert filecmp.cmp(output_md, temp_file_md), compare_files(
-            output_md, temp_file_md
-        )
+        self.check(input_md, output_md)
 
     def test_with_header(self):
         input_md = './tests/input_4.md'
         output_md = './tests/output_4.md'
-        temp_file_md = './tests/temp_ouput.md'
-        block_comment(input_md, temp_file_md)
-        print(filecmp.cmp(output_md, temp_file_md))
-        assert filecmp.cmp(output_md, temp_file_md), compare_files(
-            output_md, temp_file_md
-        )
+        self.check(input_md, output_md)
 
     def test_with_code_block(self):
         input_md = './tests/input_5.md'
         output_md = './tests/output_5.md'
-        temp_file_md = './tests/temp_ouput.md'
-        block_comment(input_md, temp_file_md)
-        print(filecmp.cmp(output_md, temp_file_md))
-        assert filecmp.cmp(output_md, temp_file_md), compare_files(
-            output_md, temp_file_md
-        )
+        self.check(input_md, output_md)
+
+    def test_header_no_label(self):
+        input_md = './tests/input_6.md'
+        output_md = './tests/output_6.md'
+        self.check(input_md, output_md)
