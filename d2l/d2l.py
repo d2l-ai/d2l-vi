2b073fa797727729ae9ea72388cccf628b1b9954
Hoa Nguyen <ntthoa.uphcm@gmail.com>
2020-05-27 21:51:06 +0200

    Using thread_pool in dataloader

--- d2l/d2l.py
+++ d2l/d2l.py
@@ -181,9 +181,9 @@ def load_data_fashion_mnist(batch_size, resize=None):
     mnist_train = dataset.FashionMNIST(train=True).transform_first(trans)
     mnist_test = dataset.FashionMNIST(train=False).transform_first(trans)
     return (gluon.data.DataLoader(mnist_train, batch_size, shuffle=True,
-                                  num_workers=get_dataloader_workers(),prefetch=2),
+                                  thread_pool=True),
             gluon.data.DataLoader(mnist_test, batch_size, shuffle=False,
-                                  num_workers=get_dataloader_workers(),prefetch=2))
+                                  num_workers=get_dataloader_workers(),prefetch=2, thread_pool=True))
 
 
 # Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
