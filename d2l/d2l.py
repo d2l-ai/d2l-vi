ab87aefb0a966c76b1acbabc2f0fad9646053978
Hoa Nguyen <ntthoa.uphcm@gmail.com>
2020-05-27 21:07:30 +0200

    change num_workers to get_dataloader_workers()

--- d2l/d2l.py
+++ d2l/d2l.py
@@ -181,9 +181,9 @@ def load_data_fashion_mnist(batch_size, resize=None):
     mnist_train = dataset.FashionMNIST(train=True).transform_first(trans)
     mnist_test = dataset.FashionMNIST(train=False).transform_first(trans)
     return (gluon.data.DataLoader(mnist_train, batch_size, shuffle=True,
-                                  num_workers=0),
+                                  num_workers=get_dataloader_workers(), timeout=None),
             gluon.data.DataLoader(mnist_test, batch_size, shuffle=False,
-                                  num_workers=0))
+                                  num_workers=get_dataloader_workers(),timeout=None))
 
 
 # Defined in file: ./chapter_linear-networks/softmax-regression-scratch.md
